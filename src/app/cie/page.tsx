733ad2b329f0a4996226e35ed0741facd449d271
rmzlb <ramzi.laieb@gmail.com>
2023-08-30 13:09:03 +0200

    update front

---
+++ src/app/cie/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+export default function Home() {
+    return (
+        <div>
+            <h1>companie</h1>
+        </div>
+    )
+  }
+
+
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>home</h1>
+    </div>
+  )
+}
